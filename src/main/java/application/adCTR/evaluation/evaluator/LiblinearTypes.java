40ba7550d288f41ffe9b519fc11e6ed78f4b5bf0
unknown <dell@D-F002528-1.1verge.com>
2013-11-19 18:05:53 +0800

    modified by zhangcen

---
+++ src/main/java/application/adCTR/evaluation/evaluator/LiblinearTypes.java
@@ -0,0 +1,22 @@
+package application.adCTR.evaluation.evaluator;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: zhangcen
+ * Date: 13-11-19
+ * Time: 下午4:11
+ */
+public enum LiblinearTypes {
+    LOGISTIC,
+    SVM;
+
+    public static boolean isLiblinearTypes(String typeName)
+    {
+        String name = typeName.toUpperCase();
+        if(name.equals("LOGISTIC") || name.equals("SVM"))
+        {
+            return true;
+        }
+        return false;
+    }
+}
